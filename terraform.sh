73595cb6735e38ceb8ab575991e9546eb7ae2d7e
ccie7599 <bapley@akamai.com>
2022-09-22 07:52:14 -0400

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,5 +1,5 @@
 sudo apt-get update &&  sudo apt-get install -y gnupg software-properties-common
 sudo apt-get install wget -y
-sudo wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor |  tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+sudo wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor |  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |  sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update &&  sudo apt-get install -y terraform
